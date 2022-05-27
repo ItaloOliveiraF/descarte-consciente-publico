6e4404cc5b1a18c022b58d42589a7997cde5ad90
Italo <itoliferandes@gmail.com>
2022-05-26 21:03:23 -0300

    base html

---
+++ components/maps/CentroConvivencia.vue
@@ -0,0 +1,3 @@
+<template>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d429.4267128363071!2d-35.20002832955138!3d-5.839525998265645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b2ff9db045f2c9%3A0xc61a8f8626026478!2sCentro%20de%20Conviv%C3%AAncia%20UFRN!5e0!3m2!1spt-BR!2sbr!4v1653514524265!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <nav>
+      <img src="" alt="Descarte Consciente">
+      <span>Início</span>
+      <span>Pontos de coleta</span>
+      <span>Informações</span>
+    </nav>
+    <section id="hero">
+      <div id="water-mask"></div>
+      <div> {{reached}} L</div>
+      <div id="progress-bar">
+        <div class="progress-background">
+          <div class="progress-background-mask"></div>
+        </div>
+      </div>
+      <div>{{progress}}%</div>
+      <div>
+        <div>Meta:</div>
+        <div>{{goal}} L</div>
+      </div>
+      <div>
+        <div>Tempo Restante</div>
+        <div>{{7}} dias, {{3}} horas e {{59}} minutos</div>
+      </div>
+    </section>
+    <section id="collection-points">
+      <MapsCentroConvivencia></MapsCentroConvivencia>
+    </section>
+    <section id="infos">
+      <div>
+        Por que descartar corretamente?
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam aliquet eleifend tortor, vitae eleifend nisi facilisis a. Pellentesque quis lacus blandit, fringilla turpis a, aliquet augue. Quisque venenatis a odio quis varius. Suspendisse dictum tempus quam tincidunt molestie. Etiam sodales pellentesque hendrerit. Suspendisse porttitor velit purus. Integer elementum sapien maximus diam lacinia viverra. Duis vel mauris sed erat dictum maximus. Maecenas suscipit, lacus sagittis ultricies luctus, ipsum neque finibus nulla, ut molestie ligula ex nec dolor. Donec sit amet diam est. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+      </div>
+
+      <div>
+        O que pode ser descartado?
+      </div>
+      <div>
+        <div>
+          Medicamentos líquidos
+        </div>
+        <div>
+          Medicamentos sólidos
+        </div>
+        <div>
+          Medicamentos semi sólidos
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "IndexPage",
+    data() {
+        return {
+            reached: 60000,
+            goal: 100000,
+            progress: 50,
+        };
+    }
+}
+</script>
+
+<style>
+#progress-bar {
+  width: 80%;
+  height: 50px;
+  background:#11353F ;
+  border: 2px solid #fff;
+  border-radius: 15px;
+
+  position: relative;
+
+  margin: 0px auto;
+}
+
+.progress-background{
+  position: relative;
+    width: 80%;
+    height: 50px;
+    border-radius: 15px;
+    background-color:blueviolet;
+}
+
+.progress-background-mask {
+  position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 50px;
+    border-radius: 15px;
+    background-image: url('@/assets/water-mask.jpg');
+    background-position: 0px;
+    background-repeat: repeat-x;
+    background-size: cover;
+
+    animation: filling 10s ease-in-out infinite;
+    opacity: 0.2;
+}
+
+@keyframes filling {
+  0% {background-position: 100px ;}
+  100% {background-position: 1000px ;}
+}
+
+#hero {
+  background: #7eba7c;
+  width:100%;
+  position: relative;
+}
+
+/* #water-mask {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('@/assets/water-mask.jpg');
+  background-size: cover;
+  background-position: 0px 2000px;
+  z-index: 0;
+  opacity: 0.2;
+} */
+</style>
